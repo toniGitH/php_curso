fef04d737a130c99b457ae34bf80ec49bb5db60f
toniGitH <marcarla1013@gmail.com>
2021-10-04 21:46:43 +0200

    Primera subida

---
+++ index.php
@@ -0,0 +1,30 @@
+<!--ESTE ARCHIVO ESTÁ ENVIADO AL REPOSITORIO-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index.php</title>
+</head>
+<body>
+    
+<?php
+
+print "Bienvenidos al curso de PHP<br>"; // Imprime y hace un salto de línea
+
+print "Hola alumnos<br>";
+
+print "Hasta el próximo vídeo";
+
+/*
+Aquí hay un comentario de
+varias líneas
+*/
+
+?>
+
+
+</body>
+</html>
